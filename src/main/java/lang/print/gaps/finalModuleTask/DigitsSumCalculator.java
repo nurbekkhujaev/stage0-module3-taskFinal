6ef1a67967874be3d70ef0000940f0bc6ba382e5
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-10-28 12:04:38 +0500

    bugs fixed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int a = number / 1000;
-        int b = (number / 100) / 10;
+        int b = (number / 100) % 10;
         int c = (number % 100) / 10;
         int d = number % 10;
 
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,13 +3,9 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
 
-        if (number >= 0) {
-            System.out.println("true");
-        }
+        boolean bool = (number > 0);
+        System.out.println(bool);
 
-        else {
-            System.out.println("false");
-        }
 
     }
 }
